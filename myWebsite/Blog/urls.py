a108dbb660467b8702f105b34721ec6d1b4c9430
suriya-ganesh <suriyaganesh097@gmail.com>
2020-02-10 20:30:04 +0530

    broken server

--- myWebsite/Blog/urls.py
+++ myWebsite/Blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.Blog, name='index'),
     path('authors/', views.authors, name='authors'),
     path('authors_simple/', views.authors_simple, name='authors'),
     path('<int:question_id>/', views.detail, name='detail'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
