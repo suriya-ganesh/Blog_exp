580f319a4c1dfda626a318c85c8c515803f47a11
suriya-ganesh <suriyaganesh097@gmail.com>
2020-03-23 05:07:27 +0530

    changed template directory.

--- website/blog/views.py
+++ website/blog/views.py
@@ -2,9 +2,11 @@ from django.views import generic
 from .models import Post
 
 class PostList(generic.ListView):
+    model = Post
+    context_object_name = 'my_post_list'
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    template_name = 'blog/index.html'
 
 class PostDetail(generic.DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'blog/post_detail.html'
