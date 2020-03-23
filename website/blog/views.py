9e7c06cbcac4bc0c28d189b77e58a3cff9ccb3c4
suriya-ganesh <suriyaganesh097@gmail.com>
2020-03-23 05:38:10 +0530

    fixed the generic.Listview not loading issue note : it was because the queryset was not working properly

--- website/blog/views.py
+++ website/blog/views.py
@@ -4,7 +4,7 @@ from .models import Post
 class PostList(generic.ListView):
     model = Post
     context_object_name = 'my_post_list'
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    queryset = Post.objects.filter(title__icontains='p')[:5]
     template_name = 'blog/index.html'
 
 class PostDetail(generic.DetailView):
