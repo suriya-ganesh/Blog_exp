23fec5c27c75f4e1d4aa0a0e1ef835e8eb69f2fc
suriya-ganesh <suriyaganesh097@gmail.com>
2020-04-02 10:55:32 +0530

    changed favicon and simplified the page

--- website/blog/views.py
+++ website/blog/views.py
@@ -5,10 +5,9 @@ from .models import Post
 class PostList(generic.ListView):
     model = Post
     context_object_name = 'my_post_list'
-    queryset = Post.objects.filter(title__icontains='p')[:5]
+    queryset = Post.objects.filter()[:5]
     template_name = 'blog/index.html'
 
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
-
