74fce915b627731f0f14072dbcf9aae240993b53
suriya-ganesh <suriyaganesh097@gmail.com>
2020-04-02 19:31:14 +0530

    completed a working 'What I'm reading' Field

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,13 +1,20 @@
 from django.views import generic
 from django.shortcuts import render
-from .models import Post
+from .models import Post,Reading
 
 class PostList(generic.ListView):
     model = Post
     context_object_name = 'my_post_list'
-    queryset = Post.objects.filter()[:5]
+    queryset = Post.objects.all()
     template_name = 'blog/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data(**kwargs)
+        context['post'] = Post.objects.all()
+        context['reading'] = Reading.objects.all()
+        return context
 
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+
